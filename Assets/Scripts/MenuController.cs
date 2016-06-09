bd96bada14ec5aca66a3ee03ae94447ff312f3f1
LNWPOR <tclnwpor@gmail.com>
2016-06-09 11:37:45 +0900

    finish ui version 0.0.0.1 and now can send variable from menu to main scene via MainManager

---
+++ Assets/Scripts/MenuController.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MenuController : MonoBehaviour {
+
+
+    public Button[] axeBtn;
+
+    void Awake()
+    {
+        SetButton();
+        initManager();
+    }
+
+    private void initManager()
+    {
+        DontDestroyOnLoad(MainManager.Instance.gameObject);
+    }
+
+    private void SetButton()
+    {
+        axeBtn[0].onClick.AddListener(() => OnClickAXE(0));
+        axeBtn[1].onClick.AddListener(() => OnClickAXE(1));
+        axeBtn[2].onClick.AddListener(() => OnClickAXE(2));
+        axeBtn[3].onClick.AddListener(() => OnClickAXE(3));
+        axeBtn[4].onClick.AddListener(() => OnClickAXE(4));
+    }
+
+    private void OnClickAXE(int axeNumber)
+    {
+        //Debug.Log(axeNumber);
+        MainManager.Instance.axeNumber = axeNumber;
+        SceneManager.LoadScene("main");
+    }
+}
