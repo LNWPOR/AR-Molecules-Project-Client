6b1c6415a468dba791ab601575a179089dac4df0
LNWPOR <tclnwpor@gmail.com>
2016-06-09 16:17:15 +0900

    init as child but still bug no texture on loadscene

--- Assets/Scripts/MainController.cs
+++ Assets/Scripts/MainController.cs
@@ -1,15 +1,39 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class MainController : MonoBehaviour {
+public class MainController : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-        Debug.Log(MainManager.Instance.axeNumber);
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    public GameObject imageTarget;
+    public GameObject[] moleculePrefabs;
+    private GameObject moleculeInstantiate;
+
+    void Awake()
+    {
+        //imageTarget = GameObject.FindGameObjectWithTag("ImageTarget");
+    }
+
+
+    void Start()
+    {
+        //for (int i = 0; i < moleculePrefabs.Length; i++)
+        //{
+        //   if (i != MainManager.Instance.axeNumber)
+        //  {
+        //     moleculePrefabs[i].SetActive(false);
+        //}
+        //}
+        //moleculePrefabs[MainManager.Instance.axeNumber].SetActive(true);
+        //Debug.Log(MainManager.Instance.axeNumber);
+
+        
+        moleculeInstantiate = Instantiate(moleculePrefabs[MainManager.Instance.axeNumber], transform.position, Quaternion.identity) as GameObject;
+        moleculeInstantiate.transform.parent = imageTarget.transform;
+        //MainManager.Instance.ARCam.SetActive(true);
+    }
+
+    public void Update()
+    {
+
+    }
 }
