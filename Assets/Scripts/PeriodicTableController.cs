1dde97196bb8760ff342f02e252f1b46d6a6cb8d
LNWPOR <tclnwpor@gmail.com>
2016-06-28 14:49:52 +0900

    init periodicTable from code for the first time

---
+++ Assets/Scripts/PeriodicTableController.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using System;
+
+public class PeriodicTableController : MonoBehaviour {
+
+    public Transform periodicTablePanel;
+    //private Button[] elementButtons;
+
+	// Use this for initialization
+	void Start () {
+        Transform[] groups = periodicTablePanel.GetComponentsInChildren<Transform>();
+        for (int i = 0; i < groups.Length;i++)
+        {
+            Button[] elementButtons = groups[i].GetComponentsInChildren<Button>();
+            for (int j = 0; j < elementButtons.Length;j++)
+            {
+                //Debug.Log(elementButtons[j].name);
+                if (PeriodicTableManager.Instance.periodicTableList.Exists(x => x.name.Contains(elementButtons[j].name)))
+                {
+                    ElementData elementForButton = PeriodicTableManager.Instance.periodicTableList.Find(x => x.name.Contains(elementButtons[j].name));
+                    Array.Find(elementButtons[j].GetComponentsInChildren<Text>(), s => s.name.Equals("NameText")).text = elementForButton.name;
+                    Array.Find(elementButtons[j].GetComponentsInChildren<Text>(), s => s.name.Equals("NumberText")).text = elementForButton.atomNumber.ToString();
+                }
+            }
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
