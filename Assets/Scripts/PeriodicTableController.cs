4b4b34b066d19ad23ff0704b6bb7e7a4ff9461ca
LNWPOR <tclnwpor@gmail.com>
2016-06-28 17:20:27 +0900

    init elementPrefab ver0.0.0.1

--- Assets/Scripts/PeriodicTableController.cs
+++ Assets/Scripts/PeriodicTableController.cs
@@ -6,15 +6,20 @@ using System;
 public class PeriodicTableController : MonoBehaviour {
 
     public Transform periodicTablePanel;
-    //private Button[] elementButtons;
+    public GameObject[] elementPrefabs;
+    private GameObject elementPrefabInitiated;
 
-	// Use this for initialization
-	void Start () {
+    void Start () {
+        initElementButton();
+    }
+
+    private void initElementButton()
+    {
         Transform[] groups = periodicTablePanel.GetComponentsInChildren<Transform>();
-        for (int i = 0; i < groups.Length;i++)
+        for (int i = 0; i < groups.Length; i++)
         {
             Button[] elementButtons = groups[i].GetComponentsInChildren<Button>();
-            for (int j = 0; j < elementButtons.Length;j++)
+            for (int j = 0; j < elementButtons.Length; j++)
             {
                 //Debug.Log(elementButtons[j].name);
                 if (PeriodicTableManager.Instance.periodicTableList.Exists(x => x.name.Contains(elementButtons[j].name)))
@@ -22,13 +27,39 @@ public class PeriodicTableController : MonoBehaviour {
                     ElementData elementForButton = PeriodicTableManager.Instance.periodicTableList.Find(x => x.name.Contains(elementButtons[j].name));
                     Array.Find(elementButtons[j].GetComponentsInChildren<Text>(), s => s.name.Equals("NameText")).text = elementForButton.name;
                     Array.Find(elementButtons[j].GetComponentsInChildren<Text>(), s => s.name.Equals("NumberText")).text = elementForButton.atomNumber.ToString();
+                    elementButtons[j].onClick.AddListener(() => OnClickElementBtn(elementForButton));
                 }
             }
         }
     }
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+
+    private void OnClickElementBtn(ElementData elementForButton)
+    {
+        //Debug.Log(elementForButton.name);
+        GameObject elementPrefabSelected = Array.Find(elementPrefabs, s => s.name.Equals(elementForButton.name));
+        if (elementPrefabSelected != null)
+        {
+            if (elementPrefabInitiated == null)
+            {
+                InstantiatePrefabSelected(elementPrefabSelected, elementForButton);
+            }
+            if (!elementPrefabInitiated.name.Equals(elementForButton.name))
+            {
+                Destroy(elementPrefabInitiated);
+                InstantiatePrefabSelected(elementPrefabSelected, elementForButton);
+            }
+        }
+        else
+        {
+            //Debug.Log("cant find prefab");
+        }
+    }
+
+    private void InstantiatePrefabSelected(GameObject elementPrefabSelected, ElementData elementForButton)
+    {
+        elementPrefabInitiated = Instantiate(elementPrefabSelected,
+                                                new Vector3(-2f, 3.5f, 0f),
+                                                Quaternion.identity) as GameObject;
+        elementPrefabInitiated.name = elementForButton.name;
+    }
 }
