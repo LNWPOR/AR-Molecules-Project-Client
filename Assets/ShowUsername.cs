eccb88da9484cec67f24877b2a063e2a71579908
praewiiezzz <modzaasard@hotmail.com>
2016-06-29 11:20:40 +1000

    Edit Sign in and Sign up Prefabs

---
+++ Assets/ShowUsername.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShowUsername : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
