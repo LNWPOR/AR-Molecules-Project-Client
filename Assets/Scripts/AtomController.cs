0020352c4683264377a1f76510042be10373ec50
LNWPOR <tclnwpor@gmail.com>
2016-06-30 17:33:33 +0900

    add rigid body to A1-A8,add destroy electron function in electron

--- Assets/Scripts/AtomController.cs
+++ Assets/Scripts/AtomController.cs
@@ -1,21 +1,37 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class AtomController : MonoBehaviour {
+public class AtomController : MonoBehaviour
+{
 
     private Vector3 rotateCenter;
-	// Use this for initialization
-	void Start () {
-        rotateCenter = new Vector3(transform.position.x -1f,
+    public bool canDestroyElectron;
+    // Use this for initialization
+    void Start()
+    {
+        rotateCenter = new Vector3(transform.position.x - 1f,
                                    transform.position.y,
                                    transform.position.z);
+        canDestroyElectron = false;
+    }
 
-
-	}
-	
-	// Update is called once per frame
-	void Update () {
-
+    // Update is called once per frame
+    void Update()
+    {
         transform.RotateAround(rotateCenter, Vector3.up, 200 * Time.deltaTime);
+    }
+
+    void OnMouseDown()
+    {
+        // this object was clicked - do something
+        Destroy(this.gameObject);
+    }
+
+    public void OnTriggerStay(Collider other)
+    {
+        if (canDestroyElectron && other.gameObject.tag.Equals("Electron"))
+        {
+            Destroy(other.gameObject);
+        }
     }
 }
