70d1be0b9ef40ce3d32dc4886751dd0e5710f250
praewiiezzz <modzaasard@hotmail.com>
2016-07-11 14:50:07 +1000

    add comment in SaveSceneButton

--- Assets/Scripts/SaveSceneButton.cs
+++ Assets/Scripts/SaveSceneButton.cs
@@ -38,7 +38,7 @@ public class SaveSceneButton : MonoBehaviour
         }
     }
 
-    public void OnClickCancelButton()
+    public void OnClickCancelButton()  // may not use
     {
         SceneManager.LoadScene("Edit");
     }
