fe531269bfc9c5c0ee46592fbf544795607dcf99
LNWPOR <tclnwpor@gmail.com>
2016-07-15 15:08:58 +0900

    add rotate auto main/test

---
+++ Assets/Scripts/MainMoleculeController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMoleculeController : MonoBehaviour {
+
+    private Vector3 rotateCenter;
+
+    // Use this for initialization
+    void Start () {
+        rotateCenter = new Vector3(transform.position.x,
+                                   transform.position.y,
+                                   transform.position.z);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        transform.RotateAround(rotateCenter, Vector3.up, 200 * Time.deltaTime);
+    }
+}
