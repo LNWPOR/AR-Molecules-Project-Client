1952df5a7e6e2a4231bce4c01154c6b5aa0f2868
LNWPOR <tclnwpor@gmail.com>
2016-07-15 14:25:22 +0900

    add delete atom button

---
+++ Assets/Scripts/PeriodicTableDeleteButton.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PeriodicTableDeleteButton : MonoBehaviour {
+
+    private GameObject periodicTableController;
+    private PeriodicTableController periodicTableControllerScript;
+
+    public GameObject mainEditMolecule;
+
+    void Awake()
+    {
+        GetPeriodicTableController();
+    }
+
+    private void GetPeriodicTableController()
+    {
+        periodicTableController = GameObject.Find("PeriodicTableController");
+        periodicTableControllerScript = periodicTableController.GetComponent<PeriodicTableController>();
+    }
+
+    public void OnClickDeleteButton()
+    {
+        Destroy(periodicTableControllerScript.GetAtomTarget());
+        periodicTableControllerScript.ClosePeriodicTable();
+        TurnOnOnClickAllAtom();
+    }
+
+    private void TurnOnOnClickAllAtom()
+    {
+        Transform[] atoms = mainEditMolecule.GetComponentsInChildren<Transform>();
+
+        foreach (Transform atom in atoms)
+        {
+            if (atom.tag.Equals("Atom"))
+            {
+                AtomController atomControllerScript = atom.GetComponent<AtomController>();
+                atomControllerScript.canClick = true;
+            }
+
+        }
+    }
+}
