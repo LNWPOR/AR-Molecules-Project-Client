c1f890f9a75d097ac44b4972c3efc4452c45d5a0
praewiiezzz <modzaasard@hotmail.com>
2016-07-18 18:25:18 +1000

    edit message box prefabs

---
+++ Assets/MessageController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+
+
+public class MessageController : MonoBehaviour {
+
+    public Button closeButton;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void OnClickOkButton()
+    {
+
+    }
+}
