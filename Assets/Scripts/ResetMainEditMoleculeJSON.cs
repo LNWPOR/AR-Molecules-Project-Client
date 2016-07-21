d62a9deedde27369e45193d21ad295ab6beca391
LNWPOR <tclnwpor@gmail.com>
2016-07-21 11:47:16 +0900

    resetMainEditMolecule onSignout and go to search from Editor Scene

---
+++ Assets/Scripts/ResetMainEditMoleculeJSON.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ResetMainEditMoleculeJSON : MonoBehaviour {
+
+	public void ResetJSON()
+    {
+        if (EditorManager.Instance.mainEditMoleculeJSON != null)
+        {
+            EditorManager.Instance.mainEditMoleculeJSON = null;
+        }
+    }
+}
