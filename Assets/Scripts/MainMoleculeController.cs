5834e9594404cf5e54eaecac58a34b2c93d629d5
praewiiezzz <modzaasard@hotmail.com>
2016-07-21 14:11:30 +0900

    Add play and pause button in Test and Main scene

--- Assets/Scripts/MainMoleculeController.cs
+++ Assets/Scripts/MainMoleculeController.cs
@@ -1,19 +1,49 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class MainMoleculeController : MonoBehaviour {
 
+    public Button playButton;
+    public Button pauseButton;
     private Vector3 rotateCenter;
+    private bool isPause;
+
 
     // Use this for initialization
     void Start () {
+        isPause = false;
+        playButton.gameObject.SetActive(false);
         rotateCenter = new Vector3(transform.position.x,
                                    transform.position.y,
                                    transform.position.z);
     }
-	
-	// Update is called once per frame
-	void Update () {
-        transform.RotateAround(rotateCenter, Vector3.up, 200 * Time.deltaTime);
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!isPause)
+        {
+            transform.RotateAround(rotateCenter, Vector3.up, 100 * Time.deltaTime);
+        }
     }
+
+    public void OnClickPauseButton()
+    {
+        isPause = true;
+        ShowPlayOrPauseButton(isPause);
+    }
+
+    public void OnClickPlayButton()
+    {
+        isPause = false;
+        ShowPlayOrPauseButton(isPause);
+    }
+
+    public void ShowPlayOrPauseButton(bool isShow)
+    {
+        playButton.gameObject.SetActive(isShow);
+        pauseButton.gameObject.SetActive(!isShow);
+    }
+
 }
